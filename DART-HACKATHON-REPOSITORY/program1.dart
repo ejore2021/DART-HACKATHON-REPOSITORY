63b832c816c0b0046a5cf5fa191c383b3314abf8
ejore2021 <161021397+ejore2021@users.noreply.github.com>
2024-03-19 09:37:08 +0300

    Add files via upload

---
+++ DART-HACKATHON-REPOSITORY/program1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Paul Ekeno Ejore";
+  int age = 49;
+  String school = "Power Learn Project";
+  String hobby = "Teaching";
+  
+  print("My name is $name. I am $age years old, studying at $school, and my hobby is $hobby.");
+}
